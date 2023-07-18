e9ba94b29320b983f6ee305b10b60036331172b0
9898s <sueh9898@icloud.com>
2023-07-18 22:16:09 +0900

    계좌 생성 API

---
+++ src/main/java/com/example/account/service/AccountService.java
@@ -0,0 +1,60 @@
+package com.example.account.service;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountUser;
+import com.example.account.dto.AccountDto;
+import com.example.account.exception.AccountException;
+import com.example.account.repository.AccountRepository;
+import com.example.account.repository.AccountUserRepository;
+import com.example.account.type.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static com.example.account.type.AccountStatus.IN_USE;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+    private final AccountUserRepository accountUserRepository;
+
+    /**
+     * 사용자가 있는지 조회
+     * 계좌에 번호를 생성하고 계좌를 저장하고 정보를 넘긴다.
+     */
+    @Transactional
+    public AccountDto createAccount(Long userId, Long initialBalance) {
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+
+        String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
+                .map(account -> String.valueOf((Integer.parseInt(account.getAccountNumber())) + 1))
+                .orElse("1000000000");
+
+        validateCreateAccount(accountUser);
+
+        return AccountDto.fromEntity(
+                accountRepository.save(Account.builder()
+                        .accountUser(accountUser)
+                        .accountStatus(IN_USE)
+                        .accountNumber(newAccountNumber)
+                        .balance(initialBalance)
+                        .registeredAt(LocalDateTime.now())
+                        .build())
+        );
+    }
+
+    private void validateCreateAccount(AccountUser accountUser) {
+        if (accountRepository.countByAccountUser(accountUser) == 10) {
+            throw new AccountException(ErrorCode.MAX_ACCOUNT_PER_USER_10);
+        }
+    }
+
+    @Transactional
+    public Account getAccount(Long id) {
+        return accountRepository.findById(id).get();
+    }
+}
